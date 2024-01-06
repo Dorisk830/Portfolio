482b6f7433f0c64b88f338eca1d7d86c42352b3a
DorisK830 <doris.kiprop@student.moringaschool.com>
2024-01-06 11:16:24 +0300

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     >
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
-          <h2 className='text text-4xl sm:text-7xl font-bold text-white'>I'm a Fullstack Developer</h2>
+          <h2 className='text text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
           <p className='text-gray-200 py-4 max-w-md text-l'>
             Experienced in web development, I've refined my skills in creating user-friendly websites and robust applications.
             Eager to bring my expertise to new challenges and contribute to innovative projects."</p>
